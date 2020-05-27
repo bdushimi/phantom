501731521e43a4e223b5fc1830dc69c781ed2a66
bdushimi <lespharaons@hotmail.com>
2020-05-27 15:51:22 +0200

    Disables cron jobs & check the number of CPU available.

--- start.js
+++ start.js
@@ -2,6 +2,7 @@
 import { config } from 'dotenv';
 import cron from 'node-cron';
 import childProcess from 'child_process';
+import os from 'os';
 import { redisConnectionError } from './services/redisService';
 import { SocketIO, trackRequests } from './services/socket';
 import eventEmitter from './services/EventEmitter';
@@ -10,32 +11,34 @@ import eventEmitter from './services/EventEmitter';
 // Start our app!
 import server from './server';
 
+console.log(`Number of CPUs available ${os.cpus().length}`);
+
 SocketIO(server);
 
 // enable dotenv configurations
 config();
 
 
-cron.schedule('*/5 * * * * *', () => {
-  if (redisConnectionError !== false) {
-    childProcess.fork('./workers/moveBus');
-  } else {
-    console.log('REDIS ERROR : ', redisConnectionError);
-  }
-});
+// cron.schedule('*/5 * * * * *', () => {
+//   if (redisConnectionError !== false) {
+//     childProcess.fork('./workers/moveBus');
+//   } else {
+//     console.log('REDIS ERROR : ', redisConnectionError);
+//   }
+// });
 
 
-cron.schedule('*/5 * * * * *', () => {
-  if (redisConnectionError !== false) {
-    const forkedProcess = childProcess.fork('./workers/sendBusData');
-    if (trackRequests.length > 0) forkedProcess.send(trackRequests);
-    forkedProcess.on('message', (data) => {
-      eventEmitter.emit('bus_updates', data.dataPoint, data.busDetails);
-    });
-  } else {
-    console.log('REDIS ERROR : ', redisConnectionError);
-  }
-});
+// cron.schedule('*/5 * * * * *', () => {
+//   if (redisConnectionError !== false) {
+//     const forkedProcess = childProcess.fork('./workers/sendBusData');
+//     if (trackRequests.length > 0) forkedProcess.send(trackRequests);
+//     forkedProcess.on('message', (data) => {
+//       eventEmitter.emit('bus_updates', data.dataPoint, data.busDetails);
+//     });
+//   } else {
+//     console.log('REDIS ERROR : ', redisConnectionError);
+//   }
+// });
 
 // Start Server
 const PORT = process.env.PORT || 5000;
