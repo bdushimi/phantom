6f9541bce05200fd1b5d36747e6e5768e644fb61
bdushimi <lespharaons@hotmail.com>
2020-05-29 01:06:05 +0200

    Specify the local address in cors

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5500');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
 // Handle Routes
 app.use('/', index);
