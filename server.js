8f23a8f5145cad8588e157c26f8d8c27af92c6b4
bdushimi <lespharaons@hotmail.com>
2020-05-29 01:11:27 +0200

    Updates the configs in cors

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5500');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
