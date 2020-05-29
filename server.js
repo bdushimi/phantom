fce771e662498c2b3855642df038663f35db2469
bdushimi <lespharaons@hotmail.com>
2020-05-29 22:29:32 +0200

    Adds one domain to cors config

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(cors({
-  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://bdushimi.github.io/phantom-demo-frontend'],
   credentials: true
 }));
 app.use(express.json());
