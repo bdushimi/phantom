f5661ffaaf73d169d91a7e30ab8d94630a226679
bdushimi <lespharaons@hotmail.com>
2020-05-29 14:11:41 +0200

    removes addionation cors configs

--- server.js
+++ server.js
@@ -16,13 +16,6 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 // Handle Routes
 app.use('/', index);
 
