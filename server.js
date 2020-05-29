34a0a825318e5e9f78eb6033a90dbaa85c5cecbd
bdushimi <lespharaons@hotmail.com>
2020-05-29 14:36:45 +0200

    Adds local address in the origin option of cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(cors({
-  origin: ['http://localhost', 'http://127.0.0.1']
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500']
 }));
 app.use(express.json());
 app.use(bodyParser.json());
