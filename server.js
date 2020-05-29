63af09c53b4a0b071352b274edfa9391eb31e73f
bdushimi <lespharaons@hotmail.com>
2020-05-29 14:35:53 +0200

    Adds local address in the origin option of cors

--- server.js
+++ server.js
@@ -11,17 +11,13 @@ const app = express();
 
 
 app.use(logger('dev'));
-// app.use(cors());
+app.use(cors({
+  origin: ['http://localhost', 'http://127.0.0.1']
+}));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', false);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 
 // Handle Routes
 app.use('/', index);
